e0ed953d877d6a2ab38f5e7a08021c7812687637
AyushSingh26 <61933844+AyushSingh26@users.noreply.github.com>
2021-09-13 11:23:55 +0530

    Update PENCIL_SKETCH.py

--- PENCIL_SKETCH.py
+++ PENCIL_SKETCH.py
@@ -1,16 +1,16 @@
 import cv2
 
-image = cv2.imread("AYUSH.jpg")
-print(image)
-cv2.imshow("AYUSH", image)
+image = cv2.imread("Lenna_(test_image).png")
+#print(image)
+#cv2.imshow("Lenna_(test_image)", image)
 cv2.waitKey(0)
 
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imshow("New Ayush", gray_image)
+#cv2.imshow("New Ayush", gray_image)
 cv2.waitKey(0)
 
 inverted_image = 255 - gray_image
-cv2.imshow("Inverted", inverted_image)
+#cv2.imshow("Inverted", inverted_image)
 cv2.waitKey()
 
 blurred = cv2.GaussianBlur(inverted_image, (21, 21), 0)
